4e015ea5b705a57c88163f2a8131436381792792
Hyeonwoo Jung <wjdgusdn1223@gmail.com>
2025-09-06 23:53:39 +0900

    test simple translator agent

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from crewai-news-reader-agent!")
+import dotenv
+
+dotenv.load_dotenv()
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
 
 
-if __name__ == "__main__":
-    main()
+@CrewBase
+class TranslatorCrew:
+
+    @agent
+    def translator_agent(self):
+        return Agent(
+            config=self.agents_config["translator_agent"],
+        )
+
+    @task
+    def translate_task(self):
+        return Task(
+            config=self.tasks_config["translate_task"],
+        )
+
+    @task
+    def retranslate_task(self):
+        return Task(
+            config=self.tasks_config["retranslate_task"],
+        )
+
+    @crew
+    def assemble_crew(self):
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            verbose=True,
+        )
+
+
+TranslatorCrew().assemble_crew().kickoff(
+    inputs={"sentence": "hi, my name is jung. i'm from korea"}
+)
